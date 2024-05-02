6a027aa61179e870db6ed2de797533ea932f7907
Govind <kumawatgovind1997@gmail.com>
2024-05-02 10:43:03 +0530

    scroll

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  experimental: {
+    taint: true,
+  },
     images: {
         domains: ['i.redd.it', 'www.api.reddit.com', 'preview.redd.it', 'external-preview.redd.it', 'files.catbox.moe', 'i.redgifs.com'],
         remotePatterns: [
